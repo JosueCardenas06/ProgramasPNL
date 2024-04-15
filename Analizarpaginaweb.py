51d3cf4039012ec078af98261c4af9571fe106a4
JosueCardenas06 <jcardenas57@ucol.mx>
2024-04-15 13:06:53 -0600

    primer commit

--- Analizarpaginaweb.py
+++ Analizarpaginaweb.py
@@ -1,8 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
+from collections import Counter
+import re
 
 # URL de la página a hacer scraping
-url = 'https://www.youtube.com/'
+url = 'https://www.wikipedia.org/'
 
 # Realizar la solicitud HTTP
 response = requests.get(url)
@@ -12,11 +14,20 @@ if response.status_code == 200:
     # Parsear el contenido HTML
     soup = BeautifulSoup(response.content, 'html.parser')
     
-    # Ejemplo: obtener todos los enlaces de la página
-    enlaces = soup.find_all('a')
+    # Encontrar todo el texto dentro de las etiquetas <p>, <h1>, <h2>, <h3>, <h4>, <h5>, <h6> y <a>
+    texto = ' '.join([p.get_text() for p in soup.find_all(['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'a'])])
     
-    # Imprimir los enlaces encontrados
-    for enlace in enlaces:
-        print(enlace.get('href'))
+    # Eliminar caracteres no alfabéticos y convertir a minúsculas
+    texto_limpio = re.sub(r'[^a-zA-Z\s]', '', texto).lower()
+    
+    # Dividir el texto en palabras
+    palabras = texto_limpio.split()
+    
+    # Contar las palabras
+    contador_palabras = Counter(palabras)
+    
+    # Imprimir las palabras y su conteo
+    for palabra, conteo in contador_palabras.items():
+        print(f'{palabra}: {conteo}')
 else:
     print('Error al cargar la página:', response.status_code)
